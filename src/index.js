9bd6cf79d5fe46a52c2c0a24b2e916bceee43ca8
Hanna Svietik <hsvietik@gmail.com>
2023-05-10 20:33:17 +0100

    best sellers layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+import { createHomeMainSection } from './js/home-best-sellers';
+
 //все нижче можна видалити, зроблено щоб бачили у консолі, що повертається
 import { getCategoryList } from './js/utils/get-category-list';
 import { getBookId } from './js/utils/get-books-id';
 import { getBooksInCategory } from './js/utils/get-books-in-category';
 import { getTopBooks } from './js/utils/get-top-books';
 
-getCategoryList()
-getTopBooks()
-const category = 'Business Books'
-getBooksInCategory(category)
-const id = '643282b1e85766588626a0d6'
-getBookId(id)
+getCategoryList();
+getTopBooks();
+const category = 'Business Books';
+getBooksInCategory(category);
+const id = '643282b1e85766588626a0d6';
+getBookId(id);

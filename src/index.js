5305e286da9dc65e40125319d3f471dedd8cefec
Igor Ievtushenko <45958058+II-777@users.noreply.github.com>
2023-05-10 02:40:18 -0400

    Add file structure to the "./src" folder

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
---
+++ src/js/support-ua.js
@@ -0,0 +1,56 @@
+export const supportArray = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: '../images/support-ua/..',
+    img2x: '../images/support-ua/..',
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: '../images/support-ua/..',
+    img2x: '../images/support-ua/..',
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: '../images/support-ua/..',
+    img2x: '../images/support-ua/..',
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: '../images/support-ua/..',
+    img2x: '../images/support-ua/..',
+  },
+  {
+    title: 'Medecins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+   img: '../images/support-ua/..',
+    img2x: '../images/support-ua/..',
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: '../images/support-ua/..',
+    img2x: '../images/support-ua/..',
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: '../images/support-ua/..',
+    img2x: '../images/support-ua/..',
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: '../images/support-ua/..',
+    img2x: '../images/support-ua/..',
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: '../images/support-ua/..',
+    img2x: '../images/support-ua/..',
+  },
+];

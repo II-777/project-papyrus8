49b5249999682aafa8232b4cd55bf11150388d30
Vasyl Shaposhnyk <aries240302@gmail.com>
2023-05-12 19:28:33 +0300

    Add details about book and fix opening card

--- src/js/book-modal.js
+++ src/js/book-modal.js
@@ -1,40 +1,63 @@
-import './utils/get-books-id';
+import { getBookId } from './utils/get-books-id';
 import * as basicLightbox from 'basiclightbox';
 
-const book = document.querySelector('.home-category-books-list');
+const book = document.querySelector('.js-home-category-books-list');
 
-book.addEventListener('click', onClick, { closable: true });
+book.addEventListener('click', onClick);
 
 function onClick(evt) {
-  //   evt.preventDefault();
+  //   console.log(evt.target);
+  const bookCard = evt.target.parentNode;
+  const bookCardId = bookCard.dataset.id;
 
-  if (evt.target.classList.contains('home-books-category-item')) {
-    return;
-  }
-  function getDetails(details) {
-    return details.map(({}) =>
+  getBookId(bookCardId).then(id => {
+    if (bookCard.classList.contains('home-books-item')) {
       basicLightbox
         .create(
           `<div class="book-modal">
-      <img src="" alt="" />
-      <h2></h2>
-      <h3></h3>
-      <p></p>
-      <ul>
-        <li>
-          <svg><use href="/src/images/icon.svg#icon-amazon"></use></svg>
-        </li>
-        <li>
-          <svg><use href="/src/images/icon.svg#icon-ibooks"></use></svg>
-        </li>
-        <li>
-          <svg><use href="/src/images/icon.svg#icon-bookshop"></use></svg>
-        </li>
-      </ul>
-      <button>Add to shopping list</button>
-    </div>`
+    <img src="${id.book_image}" alt="" />
+    <h2>${id.title}</h2>
+    <h3>${id.author}</h3>
+    <p>${id.description}</p>
+    <ul>
+      <li>
+        <svg><use href="/src/images/icon.svg#icon-amazon"></use></svg>
+      </li>
+      <li>  
+        <svg><use href="/src/images/icon.svg#icon-ibooks"></use></svg>
+      </li>
+      <li>
+        <svg><use href="/src/images/icon.svg#icon-bookshop"></use></svg>
+      </li>
+    </ul>
+    <button>Add to shopping list</button>
+  </div>`
         )
-        .show()
-    );
-  }
+        .show();
+    }
+  });
 }
+
+//   return bookCardId.map(
+//     ({
+//       book_image,
+//       title,
+//       author,
+//       amazon_product_url,
+//       book_uri,
+//       buy_links,
+//     }) => {
+// return getBookId(bookCardId).then(book => {
+
+// function getDetails(id) {
+//   getBookId(id).then(book => {
+//     book.map({
+//       book_image,
+//       title,
+//       author,
+//       amazon_product_url,
+//       book_uri,
+//       buy_links,
+//     });
+//   });
+// }

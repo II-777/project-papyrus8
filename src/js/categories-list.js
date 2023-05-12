4599ed7f3c8281793181ca3f92ce7f073000564a
Saashimiii <abaginskaya@icloud.com>
2023-05-12 21:51:51 +0300

    all categories js + update html

---
+++ src/js/categories-list.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const URL = 'https://books-backend.p.goit.global/books/category-list';
+
+const categoriesContainer = document.querySelector('.categories-list-js');
+
+//  Отримуємо масив зі списком категорії з бекенду
+const getCategoriesList = async () => {
+  const response = await axios(URL);
+  return response.data;
+};
+
+// Функція створення розмітки для меню з категоріями
+function createMurkupForCategoryList(arr) {
+  return arr
+    .map(
+      el =>
+        `<li class="categories-list__item"><a href="#">${el.list_name}</a></li>`
+    )
+    .join('');
+}
+
+// Рендер розмітки
+getCategoriesList()
+  .then(categoriesListArray =>
+    categoriesContainer.insertAdjacentHTML(
+      'beforeend',
+      createMurkupForCategoryList(categoriesListArray)
+    )
+  )
+  .catch(error => console.log(error));

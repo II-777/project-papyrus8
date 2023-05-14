11cd810c3c0f342e41940bd5f20b627831a8b306
TKazhukalo <118135022+TKazhukalo@users.noreply.github.com>
2023-05-14 15:50:13 +0300

    create-style-switcher

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,32 @@
-const switcherTheme = () => {
-  const rootStyle = document.documentElement;
-  let dataTheme = rootStyle.getAttribute('data-theme'),
-    newTheme;
-  newTheme = dataTheme === 'light' ? 'dark' : 'light';
+const toggleEl = document.querySelector('#theme-switcher');
+const bodyEl = document.querySelector('body');
+const headerEl = document.querySelector('.header__container');
+const shoppingEl = document.querySelector('.js-icon');
+const switcherEl = document.querySelector('.js-dark-switcher');
+const menuDarkEl = document.querySelector('.mobile-menu-open-icon');
+/*const btnMoreEl = document.querySelector('.home-see-more-btn');*/
+const logoEl = document.querySelector('.header__logo-name');
 
-  rootStyle.setAttribute('data-theme', newTheme);
 
-  localStorage.setItem('theme', newTheme);
-};
-document.querySelector('#theme-switcher')
-  .addEventListener('click', switcherTheme);
+/*if (defaultTheme) {
+  bodyEl.classList.add(defaultTheme);
+}*/
+
+toggleEl.addEventListener('click', switchTheme);
+
+function switchTheme(e) {
+  console.log(e.currentTarget);
+  toggleEl.classList.toggle('dark-switcher');
+    bodyEl.classList.toggle('dark-theme');
+    headerEl.classList.toggle('dark');
+    logoEl.classList.toggle('dark__icon');
+  shoppingEl.classList.toggle('dark__icon');
+  switcherEl.classList.toggle('dark-switcher');
+  menuDarkEl.classList.toggle('dark__menu');
+ /* btnMoreEl.classList.toggle('dark-button');*/
+
+
+   
+}
+
+ 

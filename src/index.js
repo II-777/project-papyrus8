0a087a82849be603f826e231eab9530382e02f53
Igor Yevtushenko <45958058+II-777@users.noreply.github.com>
2023-05-15 08:15:13 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import { createHomeMainSection } from './js/home-best-sellers';
 import './js/header';
 import './js/categories-list.js';
-// import './js/support-ua';
+import './js/support-ua';
 import './js/home-best-sellers';
 import './js/books-by-category';
-// import './js/shopping-cart';
 import './js/book-modal';
 import './js/window-reload';
 

a71eb8fb2ce31a22ab1947c12a4f78a18f8e6f8f
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-05-17 11:20:13 +0300

    add full desctop auth

--- src/js/auth/auth-listeners/on-click-sign-up.js
+++ src/js/auth/auth-listeners/on-click-sign-up.js
@@ -7,36 +7,34 @@ import { onCloseModalLogin } from "./modal-login";
 import { refs } from "../../refs-elements";
 
 export function onClickSignUp (e) {
+  refs.btnGoogle.textContent = 'SIGN UP'
+  refs.btnSignIn.classList.remove('is-active');
+  refs.btnSignUp.classList.add('is-active');
+  if(refs.inputName.getAttribute('style')){
+    refs.inputName.removeAttribute('style')
+  };
+  
+  // const username = document.querySelector('[name="username"]').value
+  // const email = document.querySelector('[name="email"]').value
+  // const password = document.querySelector('[name="password"]').value
 
-  const username = document.querySelector('[name="username"]').value
-  const email = document.querySelector('[name="email"]').value
-  const password = document.querySelector('[name="password"]').value
-
-  createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    set(ref(database, 'users/' + user.uid), {
-      username,
-      email,
-    })
-    Notify.success('Пользователь зарегистрирован!');
-    onCloseModalLogin();
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message; 
-    Notify.failure(errorMessage);
-  });
+  // createUserWithEmailAndPassword(auth, email, password)
+  // .then((userCredential) => {
+  //   const user = userCredential.user;
+  //   set(ref(database, 'users/' + user.uid), {
+  //     username,
+  //     email,
+  //   })
+  //   Notify.success('Пользователь зарегистрирован!');
+  //   onCloseModalLogin();
+  // })
+  // .catch((error) => {
+  //   const errorCode = error.code;
+  //   const errorMessage = error.message; 
+  //   Notify.failure(errorMessage);
+  // });
 };
 
-  const user = auth.currentUser;
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const uid = user.uid;
-      refs.btnOpenModalLogin.textContent = 'Log out';
-      console.log('Вошел зарегистрированный пользователь');
-      refs.headerNav.removeAttribute("hidden")
-    } else {
-    }
-  });
+
+
 
--- src/js/auth/auth.js
+++ src/js/auth/auth.js
@@ -1,3 +1,6 @@
+import { auth } from "./auth-listeners/auth-config-firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
 import { refs } from "../refs-elements";
 import { onOpenModalLogin, onCloseModalLogin } from "./auth-listeners/modal-login";
 import { onClickSignUp } from "./auth-listeners/on-click-sign-up";
@@ -9,4 +12,18 @@ refs.btnCloseModalLogin.addEventListener('click', onCloseModalLogin);
 refs.btnSignUp.addEventListener('click', onClickSignUp);
 refs.btnSignIn.addEventListener('click', onClickSignIn);
 refs.btnGoogle.addEventListener('click', onClickGoogle);
+
+const user = auth.currentUser;
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    refs.btnOpenModalLogin.textContent = 'Log out';
+    if(refs.headerNav.getAttribute('style')){
+        refs.headerNav.removeAttribute('style')
+      }
+  } else {
+    refs.headerNav.style.visibility = "hidden";
+  }
+}
+);
 

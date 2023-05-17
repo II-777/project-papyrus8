900481051de6b7d4774234136d569f54a16d575b
Hanna Svietik <hsvietik@gmail.com>
2023-05-17 01:04:56 +0100

    mobile menu on sl page is shown

--- src/shopping-cart.js
+++ src/shopping-cart.js
@@ -1,5 +1,6 @@
 import './js/header.js';
+import './js/mobile-menu.js';
 import './js/support-ua.js';
 import './js/shopping-cart.js';
-import './js/book-modal/book-modal.js';
+// import './js/book-modal/book-modal.js';
 import './js/window-reload.js';
